f102e1ea82eff8a42dd5d6610cbea2d18f255684
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-03-08 08:12:56 +0530

    added A feature to Multiple Flip

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,17 @@
-Flip=$((RANDOM%2));
+Head=1;
+Tail=0;
+Headcount=0;
+Tailcount=0;
+for (( i=1; i<10; i++));
+do
+	 Flip=$((RANDOM%2));
+		if [ $Flip -eq $Head ];
+		then
+			echo "head win"
+			((Headcount++));
+		else
+			echo "tail win"
+			((Tailcount++));
+		fi
+done
 
-if [ $Flip -eq 1 ];
-	then
-		echo "head"
-else
-		echo "tail"
-fi
